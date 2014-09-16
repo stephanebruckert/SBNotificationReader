a9f1effc72df3f580ec970b38a3a6c49d722f4ba
Stéphane Bruckert <contact@stephanebruckert.com>
2014-09-16 14:22:54 +0100

    Initial Commit

---
+++ SBNotificationReaderTests/SBNotificationReaderTests.m
@@ -0,0 +1,34 @@
+//
+//  SBNotificationReaderTests.m
+//  SBNotificationReaderTests
+//
+//  Created by Stéphane Bruckert on 16/09/14.
+//  Copyright (c) 2014 Stéphane Bruckert. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SBNotificationReaderTests : XCTestCase
+
+@end
+
+@implementation SBNotificationReaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
